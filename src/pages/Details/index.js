eef58bd541d90260f8724e2db25e4ad3af0ff568
TiagoBarbosa88 <sombr43@gmail.com>
2023-01-26 08:02:08 -0300

    update details

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -1,52 +1,46 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { APIKey } from "../../config/key";
-import { Container } from "../Home/style";
+import { Container } from "../Details/style";
+
 
 
 
 function Details() {
 
   const { id } = useParams();
-
-  const { movie, setMovies } = useState({});
+  const [ movie, setMovie ] = useState([]);
   const image_path = 'https://image.tmdb.org/t/p/w500';
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${APIKey}&language=en-US&page=1`)
-
-      .then(response => response.json())
-      .then(data => {
-
-        const { title, poster_path, overview, release_date, genres } = data;
-
-        const movie = {
-          id,
-          title,
-          sinopse: overview,
-          image: `${image_path}${poster_path}`,
-          releaseDate: release_date,
-          genero: genres
-
-        };
-
-        setMovies(movie);
-
-      });
-  }, [id]);
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${APIKey}&language=en-US`)
+    .then(response => response.json())
+    .then(data => {
+      const {title, poster_path, release_date, overview } = data
+      const movie = {
+        id,
+        title,
+        image: `${image_path}${poster_path}`,
+        sinopse: overview,
+        releaseDate: release_date,
+        
+      }
+      console.log(movie);
+      setMovie(movie)
+    })
+  }, [id])
 
   return (
     <Container>
-        <div className="movie">
-          <img src={movie.image} alt={movie.title}></img>
-          <div className="details">
-            <h1>{movie.title}</h1>
-            <span>{movie.genero}</span>
-            <span>Sinopse: {movie.sinopse}</span>
-            <span className="realese-date">Release date: {movie.releaseDate}</span>
-            <button>Go Back</button>
-          </div>
-        </div>
+      <div className="movie">
+      <img src={movie.image} alt={movie.sinopse}/>
+      <div className="details">
+        <h1>{movie.title}</h1>
+        <span>Sinopse: {movie.sinopse}</span>
+         <span className='release-date'>Release date: {movie.releaseDate}</span>
+        <Link to="/"><button>Voltar</button></Link>
+      </div>
+    </div>
     </Container>
   );
 }
